07737e74193af1089c2dc5f855e8aad2cd86ee55
Spriggan <davisraymondmuro@outlook.com>
2018-03-05 11:44:30 +0300

    Initial Commit

---
+++ bin/run.rb
@@ -0,0 +1,11 @@
+require_relative '../config/environment'
+require 'nokogiri'
+require 'open-uri'
+
+doc = Nokogiri::HTML(open("https://web.archive.org/web/20160227204808/http://flatironschool.com/team"))
+
+instructors = doc.css(".person-box")
+
+instructors.each do |instructor|
+  puts "Flatiron School <3 " + instructor.css("h2").text
+end
